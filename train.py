f6cea9cb47b55e62e27822f705ca4af7a2f83efe
SILVER66443 <2358832194@qq.com>
2020-09-29 20:15:20 +0800

    2020/09/29

--- train.py
+++ train.py
@@ -72,74 +72,74 @@ val_gennerator=test_datagen.flow_from_directory(
     batch_size=32,
     class_mode='binary')
 
-# # train data
-# history=model.fit_generator(
-#     train_gennerator,
-#     steps_per_epoch=100,
-#     epochs=50,
-#     validation_data=val_gennerator,
-#     validation_steps=100)
+# train data
+history=model.fit_generator(
+    train_gennerator,
+    steps_per_epoch=100,
+    epochs=50,
+    validation_data=val_gennerator,
+    validation_steps=100)
 
-#model.save(model_pass)
+model.save(model_pass)
 
-model.load_weights(model_pass)
+#model.load_weights(model_pass)
 #
-# # draw a map
-# acc=history.history['acc']
-# val_acc=history.history['val_acc']
-# loss=history.history['loss']
-# val_loss=history.history['val_loss']
-# epochs=range(1,len(acc)+1)
-#
-# plt.plot(epochs,acc,'bo',label="train acc")
-# plt.plot(epochs,val_acc,'b',label="val acc")
-# plt.title("train && validation's acc")
-# plt.legend()
-# plt.figure()
-#
-# plt.plot(epochs,loss,'bo',label="training loss")
-# plt.plot(epochs,val_loss,'b',label="val loss")
-# plt.title("train &&val's loss")
-# plt.legend()
-#
-# plt.show()
+# draw a map
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+epochs=range(1,len(acc)+1)
+
+plt.plot(epochs,acc,'bo',label="train acc")
+plt.plot(epochs,val_acc,'b',label="val acc")
+plt.title("train && validation's acc")
+plt.legend()
+plt.figure()
+
+plt.plot(epochs,loss,'bo',label="training loss")
+plt.plot(epochs,val_loss,'b',label="val loss")
+plt.title("train &&val's loss")
+plt.legend()
+
+plt.show()
 
 # get visual image from the net
-img=image.load_img(img_pass,target_size=[150,150])
-img_tensor=image.img_to_array(img)
-img_tensor=np.expand_dims(img_tensor,axis=0)
-img_tensor/=255.
-
-
-layer_out=[layer.output for layer in model.layers[:8]]
-activation_model=models.Model(inputs=model.input,outputs=layer_out)
-
-activations=activation_model.predict(img_tensor)
-
-layer_names=[]
-for layer in model.layers[:8] :
-    layer_names.append(layer.name)
-
-image_per_row=16
-for layer_name,layer_activation in zip(layer_names,activations):
-    n_features=layer_activation.shape[-1]
-    size=layer_activation.shape[1]
-
-    n_cols=n_features//image_per_row
-    display_grid=np.zeros((size*n_cols,image_per_row*size))
-
-    for col in range(n_cols):
-        for row in range(image_per_row):
-            channel_image=layer_activation[0,:,:,col*image_per_row+row]
-            channel_image-=channel_image.mean()
-            channel_image/=channel_image.std()
-            channel_image*=64
-            channel_image+=128
-            channel_image=np.clip(channel_image,0,255).astype('uint8')
-            display_grid[col*size:(col+1)*size,row*size:(row+1)*size]=channel_image
-    scale=1./size
-    plt.figure(figsize=(scale*display_grid.shape[1],scale*display_grid.shape[0]))
-    plt.title(layer_name)
-    plt.grid(False)
-    plt.imshow(display_grid,aspect='equal',cmap='viridis')
-
+# img=image.load_img(img_pass,target_size=[150,150])
+# img_tensor=image.img_to_array(img)
+# img_tensor=np.expand_dims(img_tensor,axis=0)
+# img_tensor/=255.
+#
+#
+# layer_out=[layer.output for layer in model.layers[:8]]
+# activation_model=models.Model(inputs=model.input,outputs=layer_out)
+#
+# activations=activation_model.predict(img_tensor)
+#
+# layer_names=[]
+# for layer in model.layers[:8] :
+#     layer_names.append(layer.name)
+#
+# image_per_row=16
+# for layer_name,layer_activation in zip(layer_names,activations):
+#     n_features=layer_activation.shape[-1]
+#     size=layer_activation.shape[1]
+#
+#     n_cols=n_features//image_per_row
+#     display_grid=np.zeros((size*n_cols,image_per_row*size))
+#
+#     for col in range(n_cols):
+#         for row in range(image_per_row):
+#             channel_image=layer_activation[0,:,:,col*image_per_row+row]
+#             channel_image-=channel_image.mean()
+#             channel_image/=channel_image.std()
+#             channel_image*=64
+#             channel_image+=128
+#             channel_image=np.clip(channel_image,0,255).astype('uint8')
+#             display_grid[col*size:(col+1)*size,row*size:(row+1)*size]=channel_image
+#     scale=1./size
+#     plt.figure(figsize=(scale*display_grid.shape[1],scale*display_grid.shape[0]))
+#     plt.title(layer_name)
+#     plt.grid(False)
+#     plt.imshow(display_grid,aspect='equal',cmap='viridis')
+#
